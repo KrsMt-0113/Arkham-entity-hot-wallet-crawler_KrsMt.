76d782aa54346d1d27ad8cee10bd5fbbae5f1b28
KrsMt-0113 <2021865415@qq.com>
2025-07-25 21:04:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# version 1.0
+# 2025.7.24
+# version: 2.0
+# 自行获取cURL存入curl.txt即可。
+# num不建议超过1500，后续可加入翻页(offset)方法
 
-import csv
-import json
+import re
 import time
-import undetected_chromedriver as uc
+import requests
+import csv
 import platform
+import sys
 import os
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
+if getattr(sys, 'frozen', False):
+    base_path = os.path.dirname(sys.executable)
+else:
+    base_path = os.path.abspath(".")
+
 
 
 def clear_console():
@@ -18,146 +23,152 @@ def clear_console():
         os.system('cls')  # Windows
     else:
         os.system('clear')  # macOS / Linux
-    title = "=== Arkham Entity Hot Wallet Crawler @ KrsMt. ==="
+    title = "========= Arkham Entity Hot Wallet Crawler @ KrsMt. ========="
     print(title)
 
-clear_console()
+def parse_curl(curl_text):
+    headers = dict(re.findall(r"-H\s+'([^:]+):\s*(.*?)'", curl_text))
 
-Chain = [
-    'bitcoin',
-    'ethereum',
-    'solana',
-    'tron',
-    'bsc',
-    'arbitrum_one',
-    'ton',
-    'polygon',
-    'dogecoin',
-    'base',
-    'sonic',
-    'optimism',
-    'mantle',
-    'avalanche',
-    'linea',
-    'blast',
-    'manta',
-    'flare'
-]
-wallets = {}
+    cookie_match = re.search(r"-b\s+'([^']+)'", curl_text)
+    cookie_str = cookie_match.group(1) if cookie_match else headers.pop('cookie', headers.pop('Cookie', ''))
 
-def auto_login(driver):
-    # 读取账号密码
-    with open("account.txt", "r", encoding="utf-8") as f:
-        lines = f.read().splitlines()
-        if len(lines) < 2:
-            print("account.txt 格式错误，应为两行：邮箱 + 密码")
-            return False
-        email, password = lines[0], lines[1]
+    cookies = {}
+    if cookie_str:
+        for pair in cookie_str.split('; '):
+            if '=' in pair:
+                k, v = pair.split('=', 1)
+                cookies[k] = v
 
-    print("正在自动登录 Arkham...")
+    return headers, cookies
 
-    driver.get("https://auth.arkm.com/login")
-    time.sleep(2)
-
-    try:
-        email_input = driver.find_element("name", "email")
-        password_input = driver.find_element("name", "password")
-        login_button = driver.find_element("xpath", '//button[@type="submit"]')
-
-        email_input.send_keys(email)
-        password_input.send_keys(password)
-        input("完成认证登录后继续...")
-        print("正在登录...")
-
-        driver.get("https://intel.arkm.com")
-        time.sleep(5)
-        login_link = driver.find_element("link text", "Login")
-        login_link.click()
-        time.sleep(5)
-
-        print("登录完成。")
-        clear_console()
-        return True
-
-    except Exception as e:
-        print(f"自动登录失败：{e}")
-        return False
-
-def fetch_json_with_selenium(driver, api_url):
-    print(f"正在获取数据...")
-    driver.get(api_url)
-    time.sleep(2)
-
-    try:
-        pre_element = driver.find_element("tag name", "pre")
-        json_text = pre_element.text
-        data = json.loads(json_text)
-
-        with open("data.json", "w", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
-
-        print("数据提取完毕。\n")
-        return data
-
-    except Exception as e:
-        print(f"获取 JSON 失败: {e}")
-        return None
-
-def extract_hot_wallet(addr_info):
+def extract_hot_wallet(addr_info, target, name):
     if (
-        addr_info.get('arkhamEntity', {}).get('name') == Entity and
+        addr_info.get('arkhamEntity', {}).get('name') == name and
         addr_info.get('arkhamLabel', {}).get('name') == 'Hot Wallet'
     ):
         address = addr_info['address']
         chain = addr_info.get('chain')
         label = addr_info['arkhamLabel']['name']
-        arkm_url = f"https://intel.arkm.com/{address}"
+        arkm_url = f"https://intel.arkm.com/explorer/address/{address}"
 
         key = f"{address}@{chain}"
-        wallets[key] = {
+        target[key] = {
             'chain': chain,
             'address': address,
             'arkm_url': arkm_url,
             'label': label
         }
 
+def fetch_chain_data(chain, entity, num, headers, cookies, Entity, offset_limit, show_chain_status=False):
+    merged_result = {}
+    limit = num
+    try:
+        for i in range(offset_limit):
+            if show_chain_status:
+                print(f"[{Entity}] 正在查询 {chain} 链的第 {i + 1} 页信息...")
+            offset = i * limit
+            pre_count = len(merged_result)
+            url = f'https://api.arkm.com/transfers?base={entity}&flow=out&usdGte=1&sortKey=time&sortDir=desc&limit={limit}&offset={offset}&tokens=&chains={chain}'
+            response = requests.get(url, headers=headers, cookies=cookies, timeout=100)
+            transfers = response.json().get('transfers')
+            if not transfers:
+                break
+            for tx in transfers:
+                extract_hot_wallet(tx.get('fromAddress', {}), merged_result, Entity)
+    except Exception as e:
+        print(f"[{chain}] 请求 offset={offset} 时出错: {e}")
+
+    return merged_result
+
+
 if __name__ == "__main__":
-    will_continue = True
-    options = uc.ChromeOptions()
-    driver = uc.Chrome(headless=False)
-    if not auto_login(driver):
-        input("自动登录失败，请手动登录后按任意键继续:")
-    while will_continue:
-        Entity = input("请输入 Entity 名称：")
-        entity = input("请输入 entity 字符串：")
-        num = input("请输入提取的 transfer 数量（如 100）：")
-        #profile_name = "SeleniumProfile"
-        #options.add_argument(f"--user-data-dir={user_data_dir}")
-        #options.add_argument(f"--profile-directory={profile_name}")
-        for chain in Chain:
-            url = f'https://api.arkm.com/transfers?base={entity}&flow=out&usdGte=1&sortKey=time&sortDir=desc&limit={num}&offset=0&tokens=&chains={chain}'
-            print(f"正在读取实体{Entity}的{chain}链热钱包信息...")
-            driver.get(url)
-            data = fetch_json_with_selenium(driver, url)
-            if data.get('transfers') is not None:
-                for tx in data['transfers']:
-                    extract_hot_wallet(tx.get('fromAddress', {}))
+    print("\033]0;Arkham Hot Wallet Crawler @ KrsMt.\007")
+    file1_path = os.path.join(base_path, "curl.txt")
 
-        result = list(wallets.values())
+    config_path = os.path.join(base_path, "config.txt")
+    num = 1000
+    offset_limit = 3
 
-        with open(f"{Entity}.csv", mode="w", encoding="utf-8", newline="") as file:
+    if os.path.exists(config_path):
+        with open(config_path, "r", encoding="utf-8") as f:
+            for line in f:
+                line = line.strip()
+                if line.startswith("num="):
+                    try:
+                        num = int(line.split("=", 1)[1])
+                    except:
+                        pass
+                elif line.startswith("offset="):
+                    try:
+                        offset_limit = int(line.split("=", 1)[1])
+                    except:
+                        pass
+
+    Chain = [
+        'bitcoin',
+        'ethereum',
+        'solana',
+        'tron',
+        'dogecoin',
+        'ton',
+        'base',
+        'arbitrum',
+        'sonic',
+        'optimism',
+        'mantle',
+        'avalanche',
+        'bsc',
+        'linea',
+        'polygon',
+        'blast',
+        'manta',
+        'flare'
+    ]
+
+    with open(file1_path, "r", encoding="utf-8") as f:
+        curl_text = f.read()
+
+    headers, cookies = parse_curl(curl_text)
+    clear_console()
+
+    args_path = os.path.join(base_path, "args.txt")
+    if not os.path.exists(args_path):
+        print("❌ 缺少 args.txt 文件，请在同目录下提供，格式为每行一个 Entity,entity")
+        sys.exit(1)
+
+    with open(args_path, "r", encoding="utf-8") as arg_file:
+        lines = [line.strip() for line in arg_file if line.strip()]
+
+    num_chains = len(Chain)
+
+    def process_entity(line, position):
+        if ',' not in line:
+            print(f"❌ 格式错误：{line}")
+            return
+        Entity, entity = [x.strip() for x in line.split(',', 1)]
+        result = {}
+
+        for idx, chain in enumerate(Chain, 1):
+            print(f"\n[{Entity}] 正在查询第 {idx}/18 条链：{chain}...")
+            partial = fetch_chain_data(chain, entity, num, headers, cookies, Entity, offset_limit, show_chain_status=True)
+            result.update(partial)
+            print(f"[{Entity}] {chain} 链共找到 {len(partial)} 个热钱包。")
+            time.sleep(1)
+            clear_console()
+
+        # 按 Chain 顺序排序
+        result = [result[key] for chain in Chain for key in result if result[key]['chain'] == chain]
+
+        os.makedirs(os.path.join(base_path, "result"), exist_ok=True)
+        file2_path = os.path.join(base_path, "result", f"{Entity}.csv")
+
+        with open(file2_path, mode="w", encoding="utf-8", newline="") as file:
             fieldnames = ['chain', 'address', 'arkm_url', 'label']
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(result)
-            print(f"结果已保存为{Entity}.csv")
 
-        while True:
-            will_continue_input = input("是否继续获取数据？(y/n)")
-            if will_continue_input == 'y':
-                break
-            elif will_continue_input == 'n':
-                will_continue = False
-                exit()
-            else:
-                print("无效输入。\n")
+    for i, line in enumerate(lines):
+        process_entity(line, i)
+
+    sys.exit()
